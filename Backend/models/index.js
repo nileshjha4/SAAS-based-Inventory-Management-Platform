81d4d7f1b021f33936c86be61bee9af92b7346f7
Nilesh Jha <nileshjha412@gmail.com>
2025-02-02 11:09:13 +0530

    update mongo db connection uri

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-// const uri = "mongodb+srv://nileshjha71:30RBzPKvItIU4Mmr@cluster0.jil4r.mongodb.net/Beverage_Marketplace?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://nileshjha71:30RBzPKvItIU4Mmr@cluster0.jil4r.mongodb.net/Beverage_Marketplace?retryWrites=true&w=majority&appName=Cluster0";
 
-const uri = 'mongodb://localhost:27017/Beverages2'
+// const uri = 'mongodb://localhost:27017/Beverages2'
 
 
 function main() {
